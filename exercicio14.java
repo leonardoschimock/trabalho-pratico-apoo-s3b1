09b81769be37c26acfea5031b890ab9bfaa6ef0c
ggJeans <Siqjean1331@gmail.com>
2026-03-16 21:00:03 -0300

    Implement tabuada calculation in exercicio14.java
    O programa solicita que o usuário digite um número inteiro e exibe sua tabuada de 1 a 10.

--- exercicio14.java
+++ exercicio14.java
@@ -32,3 +32,10 @@ public class exercicio14 {
         scanner.close();
     }
 }
+
+/***************************************************************** 
+ O programa solicita que o usuário digite um número inteiro, 
+ em seguida, utiliza um laço de repetição (for) para calcular a tabuada desse número de 1 até 10.
+ A cada repetição, o programa multiplica o número digitado pelo valor atual do contador (i) e mostra o resultado na tela.
+ Dessa forma, ao final do laço, toda a tabuada do número é exibida.
+ ********************************************************************/
